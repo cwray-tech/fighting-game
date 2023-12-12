841d64f1ea35fd4f8d857c4eb853031a80e2d74a
Christopher Wray <chris@sol.company>
2023-12-11 19:58:26 -0800

    setup project

---
+++ index.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+context.fillRect(0, 0, canvas.width, canvas.height);
