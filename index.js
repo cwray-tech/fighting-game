721115483f6595a44d66677c67ff3655c53c5289
Christopher Wray <chris@sol.company>
2023-12-15 16:20:23 -0800

    add player run animation

--- index.js
+++ index.js
@@ -41,6 +41,16 @@ const player = new Fighter({
     x: 215,
     y: 157,
   },
+  sprites: {
+    idle: {
+      imageSrc: "./img/samuraiMack/Idle.png",
+      framesMax: 8,
+    },
+    run: {
+      imageSrc: "./img/samuraiMack/Run.png",
+      framesMax: 8,
+    },
+  },
 });
 
 const enemy = new Fighter({
@@ -89,10 +99,13 @@ function animate() {
   player.velocity.x = 0;
   enemy.velocity.x = 0;
   // player movement
+  player.image = player.sprites.idle.image;
   if (keys.a.pressed && player.lastKey == "a") {
     player.velocity.x = -5;
+    player.image = player.sprites.run.image;
   } else if (keys.d.pressed && player.lastKey == "d") {
     player.velocity.x = 5;
+    player.image = player.sprites.run.image;
   }
   // enemy movement
   if (keys.ArrowLeft.pressed && enemy.lastKey == "ArrowLeft") {
